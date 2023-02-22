96daaa7a6effdbf13ad6db75a717aa9729b34322
Andrey Paskal <andrey@paskal.email>
2023-02-22 22:53:20 +0400

    fix: This commit fixes deprication of '.update_capabilities' warning

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -94,7 +94,7 @@ cmp.setup.cmdline(':', {
 
 
 require('lspconfig')['tsserver'].setup {
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
   settings = {
     diagnostics = {
       -- Ignore some diagnostics messages
